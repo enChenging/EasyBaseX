30e2f7df066a74ed57202adc8ba9f34071c60457
chengyancheng <2416677458@qq.com>
2021-05-13 17:42:31 +0800

    simple 中新增CustomAutoCompleteTextView

--- simplex/src/main/java/com/release/simplex/ui/act/AutoCompleteTextViewActivity.java
+++ simplex/src/main/java/com/release/simplex/ui/act/AutoCompleteTextViewActivity.java
@@ -3,13 +3,15 @@ package com.release.simplex.ui.act;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
 
+import com.orhanobut.logger.Logger;
 import com.release.simplex.R;
 import com.release.simplex.ui.adapter.AutoCompleteAdapter;
 import com.release.simplex.widget.CustomAutoCompleteTextView;
 
 import java.util.Arrays;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,9 +28,24 @@ public class AutoCompleteTextViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auto_complete_text_view);
         userName = findViewById(R.id.userName);
         View cl = findViewById(R.id.cl);
+        List data = Arrays.asList(strArray);
 
 
-        initAutoComplete();
+        adapter = new AutoCompleteAdapter(AutoCompleteTextViewActivity.this, data);
+        userName.setAdapter(adapter);
+
+        userName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) adapter.getItem(position);
+                Logger.i("position:" + position + "  item:" + item);
+                for (int i = 0; i < data.size(); i++) {
+                    if (item.equals(data.get(i))){
+                        Logger.i("currentPosition："+i);
+                    }
+                }
+            }
+        });
 
         cl.setOnTouchListener(new View.OnTouchListener() {
 
@@ -42,10 +59,4 @@ public class AutoCompleteTextViewActivity extends AppCompatActivity {
         });
     }
 
-
-    private void initAutoComplete() {
-
-        adapter = new AutoCompleteAdapter(AutoCompleteTextViewActivity.this, Arrays.asList(strArray));
-        userName.setAdapter(adapter);
-    }
 }
